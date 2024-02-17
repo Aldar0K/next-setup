4b36b79a7913f61d59d93406f09b3eee1bacf3b2
Aldar <aldarokonov21@gmail.com>
2024-02-17 14:01:42 +0300

    fix: resolve name conflicts

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,9 +1,9 @@
 export { AppLink, AppLinkThemes } from './app-link';
-export { Button, ButtonSizes, ButtonThemes } from './button-rename';
-export { Input } from './input-rename';
-export { Loader } from './loader-rename';
-export { Modal } from './modal-rename';
-export { Text } from './text-rename';
+export { Button, ButtonSizes, ButtonThemes } from './button';
+export { Input } from './input';
+export { Loader } from './loader';
+export { Modal } from './modal';
+export { Text } from './text';
 
 export { IconAbout } from './icons/IconAbout';
 export { IconMain } from './icons/IconMain';
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,4 +1,4 @@
-export { Header } from './header-rename';
+export { Header } from './header';
 export { PageError } from './page-error';
 export { PageLoader } from './page-loader';
 export { Sidebar } from './sidebar';
