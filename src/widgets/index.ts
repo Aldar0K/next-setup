722da253fa29cd5c65ce8fbb1a8f5d820864890e
Aldar <aldarokonov21@gmail.com>
2024-02-17 13:59:00 +0300

    fix: resolve name conflicts

--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -1,4 +1,4 @@
-export { Header } from './header';
+export { Header } from './header-rename';
 export { PageError } from './page-error';
 export { PageLoader } from './page-loader';
 export { Sidebar } from './sidebar';
